e753169dae291675a8e73d8d117c45e5959fec24
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-10-20 12:40:19 -0400

    Remove initial request group

--- test/test.js
+++ test/test.js
@@ -22,33 +22,30 @@ describe("DBS Arweave Upload", () => {
 
     describe("upload", () => {
 
-        describe("Initial request", () => {
-
-            it("should pull funds from user account.", async () => {
-                const quoteResponse = await axios.post(`http://localhost:8081/getQuote`, {
-                    type: "arweave",
-                    userAddress: wallet.address,
-                    files: [{length: 1256}, {length: 5969}],
-                    payment: {
-                        chainId: 80001,
-                        tokenAddress: "0x0000000000000000000000000000000000001010",
-                    },
-                });
-                const quote = quoteResponse.data;
-
-                nonce = Math.floor(new Date().getTime()) / 1000;
-                message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
-                signature = await wallet.signMessage(message);
-
-                const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
-                    quoteId: quote.quoteId,
-                    files: ["https://example.com/", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"],
-                    nonce: nonce,
-                    signature: signature,
-                });
-
-                expect(uploadResponse.status).to.be.equal(200);
+        it("should pull funds from user account.", async () => {
+            const quoteResponse = await axios.post(`http://localhost:8081/getQuote`, {
+                type: "arweave",
+                userAddress: wallet.address,
+                files: [{length: 1256}, {length: 5969}],
+                payment: {
+                    chainId: 80001,
+                    tokenAddress: "0x0000000000000000000000000000000000001010",
+                },
             });
+            const quote = quoteResponse.data;
+
+            nonce = Math.floor(new Date().getTime()) / 1000;
+            message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
+            signature = await wallet.signMessage(message);
+
+            const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
+                quoteId: quote.quoteId,
+                files: ["https://example.com/", "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"],
+                nonce: nonce,
+                signature: signature,
+            });
+
+            expect(uploadResponse.status).to.be.equal(200);
         });
     });
 
