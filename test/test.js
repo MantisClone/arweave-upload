f49056214300173b8c2585895d6db59251a80ace
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-10-21 14:43:06 -0400

    Reorganize tests

--- test/test.js
+++ test/test.js
@@ -17,81 +17,87 @@ describe("DBS Arweave Upload", function () {
     });
 
     describe("Integration tests", function () {
-        const abi = [
-            'function approve(address, uint256) external returns (bool)',
-        ];
-        const token = new ethers.Contract("0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", abi, wallet);
 
-        afterEach(async function () {
-            const serverWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-            // Revoke approval
-            await (await token.approve(serverWallet.address, ethers.BigNumber.from(0))).wait(15);
-        });
+        describe("without approval", function () {
 
-        it("should fail to pull funds from user account without approval", async function() {
-            this.timeout(20000);
+            it("should fail to pull funds from user account", async function() {
+                this.timeout(20000);
 
-            const quoteResponse = await getQuote(wallet);
-            const quote = quoteResponse.data;
+                const quoteResponse = await getQuote(wallet);
+                const quote = quoteResponse.data;
 
-            const nonce = Math.floor(new Date().getTime()) / 1000;
-            const message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
-            const signature = await wallet.signMessage(message);
-            const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
-                quoteId: quote.quoteId,
-                files: ["ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi", "ipfs://QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx"],
-                nonce: nonce,
-                signature: signature,
+                const nonce = Math.floor(new Date().getTime()) / 1000;
+                const message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
+                const signature = await wallet.signMessage(message);
+                const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
+                    quoteId: quote.quoteId,
+                    files: ["ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi", "ipfs://QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx"],
+                    nonce: nonce,
+                    signature: signature,
+                });
+                expect(uploadResponse).to.exist;
+                expect(uploadResponse.status).to.equal(200);
+
+                let status
+                for(let i = 0; i < 15; i++) {
+                    let getStatusResponse = await axios.get(`http://localhost:8081/getStatus?quoteId=${quote.quoteId}`);
+                    expect(getStatusResponse).to.exist;
+                    expect(getStatusResponse.status).to.equal(200);
+                    status = getStatusResponse.data.status;
+                    console.log(`status = ${status}`);
+                    if(status === 5) break;
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+                expect(status).to.be.equal(6);
             });
-            expect(uploadResponse).to.exist;
-            expect(uploadResponse.status).to.equal(200);
+        })
 
-            let status
-            for(let i = 0; i < 15; i++) {
-                let getStatusResponse = await axios.get(`http://localhost:8081/getStatus?quoteId=${quote.quoteId}`);
-                expect(getStatusResponse).to.exist;
-                expect(getStatusResponse.status).to.equal(200);
-                status = getStatusResponse.data.status;
-                console.log(`status = ${status}`);
-                if(status === 5) break;
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-            expect(status).to.be.equal(6);
-        });
+        describe("with approval", function () {
 
-        it("should pull funds from user account with approval", async function() {
-            this.timeout(20000);
+            const abi = [
+                'function approve(address, uint256) external returns (bool)',
+            ];
+            const token = new ethers.Contract("0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889", abi, wallet);
 
-            const quoteResponse = await getQuote(wallet);
-            const quote = quoteResponse.data;
-
-            // Grant infinite approval
-			await (await token.approve(quote.approveAddress, ethers.constants.MaxInt256)).wait(15);
-
-            const nonce = Math.floor(new Date().getTime()) / 1000;
-            const message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
-            const signature = await wallet.signMessage(message);
-            const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
-                quoteId: quote.quoteId,
-                files: ["ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi", "ipfs://QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx"],
-                nonce: nonce,
-                signature: signature,
+            afterEach(async function () {
+                const serverWallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+                // Revoke approval
+                await (await token.approve(serverWallet.address, ethers.BigNumber.from(0))).wait(15);
             });
-            expect(uploadResponse).to.exist;
-            expect(uploadResponse.status).to.equal(200);
 
-            let status
-            for(let i = 0; i < 15; i++) {
-                let getStatusResponse = await axios.get(`http://localhost:8081/getStatus?quoteId=${quote.quoteId}`);
-                expect(getStatusResponse).to.exist;
-                expect(getStatusResponse.status).to.equal(200);
-                status = getStatusResponse.data.status;
-                console.log(`status = ${status}`);
-                if(status === 5) break;
-                await new Promise(resolve => setTimeout(resolve, 1000));
-            }
-            expect(status).to.be.equal(6);
+            it("should successfully pull funds from user account", async function() {
+                this.timeout(20000);
+
+                const quoteResponse = await getQuote(wallet);
+                const quote = quoteResponse.data;
+
+                // Grant infinite approval
+                await (await token.approve(quote.approveAddress, ethers.constants.MaxInt256)).wait(15);
+
+                const nonce = Math.floor(new Date().getTime()) / 1000;
+                const message = ethers.utils.sha256(ethers.utils.toUtf8Bytes(quote.quoteId + nonce.toString()));
+                const signature = await wallet.signMessage(message);
+                const uploadResponse = await axios.post(`http://localhost:8081/upload`, {
+                    quoteId: quote.quoteId,
+                    files: ["ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi", "ipfs://QmZ4tDuvesekSs4qM5ZBKpXiZGun7S2CYtEZRB3DYXkjGx"],
+                    nonce: nonce,
+                    signature: signature,
+                });
+                expect(uploadResponse).to.exist;
+                expect(uploadResponse.status).to.equal(200);
+
+                let status
+                for(let i = 0; i < 15; i++) {
+                    let getStatusResponse = await axios.get(`http://localhost:8081/getStatus?quoteId=${quote.quoteId}`);
+                    expect(getStatusResponse).to.exist;
+                    expect(getStatusResponse.status).to.equal(200);
+                    status = getStatusResponse.data.status;
+                    console.log(`status = ${status}`);
+                    if(status === 5) break;
+                    await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+                expect(status).to.be.equal(6);
+            });
         });
     });
-
 });
