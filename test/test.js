f49c3e47ae2034a37616c2af969789c19a4e2efc
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-10-21 16:12:39 -0400

    Fix tests

--- test/test.js
+++ test/test.js
@@ -45,7 +45,7 @@ describe("DBS Arweave Upload", function () {
                     expect(getStatusResponse.status).to.equal(200);
                     status = getStatusResponse.data.status;
                     console.log(`status = ${status}`);
-                    if(status === 5) break;
+                    if(status >= 5) break;
                     await new Promise(resolve => setTimeout(resolve, 1000));
                 }
                 expect(status).to.be.equal(6);
