32aa6895f68e3e651f93b4672b1e844dfa288e99
MantisClone <david.hunt-mateo@mantisdata.solutions>
2022-10-25 15:12:14 -0400

    Make errorResponse not const

--- app/controllers/error.js
+++ app/controllers/error.js
@@ -1,4 +1,4 @@
-const errorResponse = (res, statusCode, message) => {
+errorResponse = (res, statusCode, message) => {
     console.error(`error: ${statusCode}: ${message}`);
     res.status(statusCode).send({message: message});
 }
